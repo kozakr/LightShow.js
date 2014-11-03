166c98e986248c4e5e9cc4874b00c47e2c651b33
Roman Kozák <rmnkzk@gmail.com>
2014-11-03 10:51:46 +0100

    Update to v0.5-beta
    LightShow can now change url (e.g. #slide2) so that you are able to send
    someone link of the exact slide you want

--- source/jquery.lightshow.js
+++ source/jquery.lightshow.js
@@ -1,5 +1,5 @@
 /*
- * LightShow.js v0.4
+ * LightShow.js v0.5-beta
  * http://romankozak.cz 
  *
  * Copyright (c) 2014 Roman Kozák
@@ -17,16 +17,30 @@
       transition   : "fade",  // (string)  transition between slides 
       controls     : true,    // (boolean) show controls 
       big_controls : false,   // (boolean) big controls - half of an image
-      title        : false    // (boolean) show title from 'data-title' attribute of <li>
+      title        : false,   // (boolean) show title from 'data-title' attribute of <li>
+      change_url   : false    // (boolean) put current slide number into url
     }, options);
 
-    this.each(function() {
+    this.each(function(index) {
+        console.log(index);
       var $this = $(this);
 
       var content = $this.find('ul');
+      var lis = content.find('li');
 
       $this.addClass("lightshow-wrapper");
-      $this.find('li:first-child').show();
+      
+      lis.each(function(index) {
+        $(this).data("index", index);
+      });
+
+      if(settings.change_url){
+        var i = window.location.hash.substr(6);
+        content.find('li:eq('+i+')').show();
+        content.find('li:lt('+i+')').appendTo(content);
+      } else {
+        content.find('li:first-child').show();
+      }
 
       // autoplay
       if(settings.autoplay) {
@@ -34,21 +48,22 @@
         setInterval(function() { 
           if(!(settings.pause && $this.is(':hover'))) {
             var first = content.find('li:first-child');
-            switch(settings.transition)
-            {
+            var next = first.next('li');
+
+            switch(settings.transition) {
               case "fade":
                 first
                   .fadeOut(settings.animation);
                 break;
               case "slide":
                 first
-                  .animate({ left: "-100%" }, settings.animation, function() { $(this).css("left", "0").hide(); })
-                  .next('li').css("left", "100%").show().animate({ left: "0" }, settings.animation);
+                  .animate({ left: "-100%" }, settings.animation, function() { $(this).css("left", "0").hide(); });
+                next.css("left", "100%").show().animate({ left: "0" }, settings.animation);
                 break;
             }
-            first
-              .next('li').show()
-              .end().appendTo(content);
+            next.show();
+            first.appendTo(content);
+            if(settings.change_url) window.location.hash = "#slide" + next.data('index');
           }
         }, settings.duration);
       }
@@ -58,14 +73,14 @@
         var sirka = $this.find("li").outerWidth();
         var vyska = $this.find("li").outerHeight();
 
-        var next = $("<div/>", { class: "lightshow-next" }).appendTo($this);
-        var prev = $("<div/>", { class: "lightshow-prev" }).appendTo($this);
+        var button_next = $("<div/>", { class: "lightshow-next" }).appendTo($this);
+        var button_prev = $("<div/>", { class: "lightshow-prev" }).appendTo($this);
         
         var top = (vyska/2 - 30) + "px";
         if(top > 0)
         {
-          next.css("top", top);
-          prev.css("top", top);
+          button_next.css("top", top);
+          button_prev.css("top", top);
         }
 
         if(settings.big_controls)
@@ -75,14 +90,16 @@
             width: (sirka/2)-20 + "px"
           }
 
-          next.addClass("lightshow-big").css(css);
-          prev.addClass("lightshow-big").css(css);
+          button_next.addClass("lightshow-big").css(css);
+          button_prev.addClass("lightshow-big").css(css);
         }
 
-        next.click(function() {
+        button_next.click(function() {
           if(!content.find('li').is(':animated'))
           {
             var first = content.find('li:first-child');
+            var next = first.next('li');
+
             switch(settings.transition)
             {
               case "fade":
@@ -95,16 +112,18 @@
                   .next('li').css("left", "100%").show().animate({ left: "0" }, settings.animation);
                 break;
             }
-            first
-              .next('li').show()
-              .end().appendTo(content);
+            next.show();
+            first.appendTo(content);
+            if(settings.change_url) window.location.hash = "#slide" + next.data('index');
           }
         });
 
-        prev.click(function() {
+        button_prev.click(function() {
           if(!content.find('li').is(':animated'))
           {
             var first = content.find('li:first-child');
+            var prev = content.find('li:last-child');
+
             switch(settings.transition)
             {
               case "fade":
@@ -117,8 +136,8 @@
                   content.find('li:last-child').css("left", "-100%").show().animate({ left: "0" }, settings.animation);
                 break;
             }
-            content.find('li:last-child').show()
-              .prependTo(content);
+            prev.show().prependTo(content);
+            if(settings.change_url) window.location.hash = "#slide" + prev.data('index');
           }
         });
       }
